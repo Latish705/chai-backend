688ac0d29d615e1de12c3235e4e5758e20cd19ae
Latish 705 <104522651+Latish705@users.noreply.github.com>
2024-01-08 00:28:04 +0000

    Add video retrieval functionality to dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,10 +8,22 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const user = req.user
+    const allVideos =await Video.findById(user._id)
+    if(!allVideos){
+        throw new ApiError(400,"Error fetching all the video of channel")
+    }
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const user = req.user
+    const videos = await Video.find(user._id)
+    if(!videos){
+        throw new ApiError(400,"Cannot get all the video of user")
+    }
+    res.status(200).json(new ApiResponse(200,{videos},"videos fetched successfully"))
 })
 
 export {
